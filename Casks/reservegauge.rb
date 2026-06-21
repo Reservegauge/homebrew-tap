0d368452cbb88e71d29be33ae67405279729b87a
JL Wright <jlwright@JLs-MacBook-Pro.local>
2026-06-21 12:58:15 +0100

    reservegauge 1.0.45

--- Casks/reservegauge.rb
+++ Casks/reservegauge.rb
@@ -1,6 +1,6 @@
 cask "reservegauge" do
-  version "1.0.44"
-  sha256 "8f16cdecd68b560ff0ef8292e51a968efa7497a1c8f437027268a4eebdadf3d1"
+  version "1.0.45"
+  sha256 "6d1b09f9c2f6becb2427efcfa32eefeaaf1e831e5eb830012b158c23049e7d45"
 
   url "https://reservegauge.com/ReserveGauge.dmg"
   name "ReserveGauge"
