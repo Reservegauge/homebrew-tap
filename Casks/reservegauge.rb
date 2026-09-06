63c69532271b919630c24709e3a8d38e13bf329e
JL Wright <jlucwright@gmail.com>
2026-09-06 11:28:17 +0100

    reservegauge 1.0.102

--- Casks/reservegauge.rb
+++ Casks/reservegauge.rb
@@ -1,6 +1,6 @@
 cask "reservegauge" do
-  version "1.0.101"
-  sha256 "3f6010aa87e2fb7de24766c177b6e31233303614ba7d96ac6d02dc019c667cd5"
+  version "1.0.102"
+  sha256 "d4b41cdbb72a65975eccd5354abd8e06d48019ea49b37f5229a74f47bd17649d"
 
   url "https://reservegauge.com/ReserveGauge.dmg"
   name "ReserveGauge"
